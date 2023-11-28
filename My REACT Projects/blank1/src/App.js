c9ce91a109f4a7901452d711b4da66ddbf989a88
Siddharth Vekariya <siddharthvekariya510@gmail.com>
2023-11-28 13:04:45 +0530

    28112023

--- My REACT Projects/blank1/src/App.js
+++ My REACT Projects/blank1/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react'
+import { Routes, Route } from 'react-router-dom'
+import ToDoList from './Components/ToDoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Routes>
+        <Route to={"/"} element={<ToDoList />}></Route>
+      </Routes> */}
+      <ToDoList />
+    </>
   );
 }
 
